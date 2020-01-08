9e7caa30d07ec0b054438618c2e7e29a6baca788
1612842 <1612842@student.hcmus.edu.vn>
2020-01-08 14:08:55 +0700

    Move the bird

--- Assets/Script/birdController.cs
+++ Assets/Script/birdController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class birdController : MonoBehaviour
 {
+    private Transform targetFocus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetFocus = GameObject.FindGameObjectWithTag("target").transform;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 target = targetFocus.position - this.transform.position;
+
+        transform.LookAt(targetFocus.transform);
     }
 }
